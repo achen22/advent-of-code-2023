ec0a90bac9517a8495cf41ee8e56c09150d6e3ef
Alan <alanchen.2003@gmail.com>
2023-12-09 16:21:13 +1100

    Day 9

---
+++ day09-2.py
@@ -0,0 +1,26 @@
+import sys
+import itertools
+
+if __name__ == "__main__":
+    history = []
+
+    filename = "input" if len(sys.argv) == 1 else sys.argv[1]
+    with open(filename) as f:
+        for line in f.readlines():
+            history.append([int(n) for n in line.rstrip().split(" ")])
+
+    total = 0
+    for values in history:
+        start = values[0]
+        diffs = [values]
+        diff = [b - a for a, b in itertools.pairwise(values)]
+        while len(diff) > 1 and any(d != diff[0] for d in diff[1:]):
+            diffs.append(diff)
+            diff = [b - a for a, b in itertools.pairwise(diff)]
+        prev = diff[0]
+        for d in reversed(diffs):
+            prev = d[0] - prev
+        #print(prev)
+        total += prev
+
+    print(total)
